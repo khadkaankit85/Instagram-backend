d66bc5f50ca75c9e2145d72f90162b833bddc460
Angkit Khadka <khadkaankit85@gmail.com>
2024-09-27 13:36:48 -0400

    converted into module system

--- src/Controllers/createAccount.ts
+++ src/Controllers/createAccount.ts
@@ -1,28 +1,23 @@
-// const { json } = require("body-parser")
-// const { readFile, readFileSync } = require("fs")
-// const path = require("path")
-// const fs = require("fs").promises
+import { json } from "body-parser";
+import { readFile, readFileSync } from "fs";
+import path from "path";
+import { promises as fs } from "fs";
 
-// let userInformation = {
-//     id: 1,
-//     firstName: "",
-//     lastName: "",
-//     email: "",
-//     gender: "Intermediate",
-//     username: ""
-// }
+let userInformation = {
+    id: 1,
+    firstName: "",
+    lastName: "",
+    email: "",
+    gender: "Intermediate",
+    username: ""
+};
 
+async function userExists(username) {
+    try {
+        // Implementation here
+    } catch (e) {
+        console.log("error while reading the file", e);
+    }
+}
 
-// async function userExists(username) {
-
-//     try {
-
-//     }
-//     catch (e) {
-//         console.log("error while reading the file", e)
-//     }
-// }
-
-
-
-
+export { userInformation, userExists };
